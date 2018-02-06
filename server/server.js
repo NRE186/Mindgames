d8c0c989e6646a03a93245743aff9339889e972e
Web_Developer186 <TheTroller186@gmail.com>
2018-02-06 18:37:45 +0500

    New files system

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const history = require('connect-history-api-fallback');
+const path = require('path');
+const serveStatic = require('serve-static');
 
 const app = express();
 const port = process.env.PORT || 1488;
@@ -9,8 +12,11 @@ const port = process.env.PORT || 1488;
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(history());
+app.use(serveStatic(path.join(__dirname, '..')))
 
-app.post('/', (req, res) => {
+
+app.post('/api/register', (req, res) => {
     res.send({
         message: `Hello ${req.body.email}! Your user was registered! Have fun!`
     })
